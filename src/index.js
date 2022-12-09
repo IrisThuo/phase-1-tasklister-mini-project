acb2265eb13df5b709681daf4ebf9ee39c7823ec
Iris <iris.thuo@student.moringaschool.com>
2022-12-09 09:48:40 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form =document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+   e.preventDefault()
+  handleToDo(e.target.querySelector('#new-task-description').value)
+   })
 });
+
+function handleToDo(todo){
+  let p = document.createElement('p')
+  let btn= document.createElement('button')
+  btn.addEventListener('click', deleteBtn)
+  btn.textContent= 'x'
+  p.textContent= `${todo} `
+  p.appendChild(btn)
+  
+  document.querySelector('#tasks').appendChild(p)
+
+}
+
+function deleteBtn(e){
+e.target.parentNode.remove()
+}
